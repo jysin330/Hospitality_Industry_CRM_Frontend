cab80e3359dcb80a90fc633653b2e6d2a905011c
Jyoti <jyotisingh82000@gmail.com>
2024-02-10 13:24:03 +0530

    importing { Line, Bar, Doughnut } from 'react-chartjs-2 and the related function created

--- crm_frontend_app/src/components/MetricsPage.js
+++ crm_frontend_app/src/components/MetricsPage.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Line, Bar, Doughnut } from 'react-chartjs-2';
 
 const MetricsPage = () => {
   const [selectedCity, setSelectedCity] = useState('');
-  
+  const [netSaleData, setNetSaleData] = useState([]);
+  const [netExpenseData, setNetExpenseData] = useState([]);
+  const [targetRemainingData, setTargetRemainingData] = useState([]);
+
   useEffect(() => {
     if (selectedCity) {
       axios.get(`http://127.0.0.1:8000//metrics/?city=${selectedCity}`)
@@ -19,6 +22,58 @@ const MetricsPage = () => {
         });
     }
   }, [selectedCity]);
+  
+  const netSaleChart = {
+    labels: Array.from({ length: netSaleData.length }, (_, i) => i + 1),
+    datasets: [
+      {
+        label: 'Net Sale',
+        data: netSaleData,
+        fill: false,
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+
+  const netExpenseChart = {
+    labels: Array.from({ length: netExpenseData.length }, (_, i) => i + 1),
+    datasets: [
+      {
+        label: 'Net Expense',
+        data: netExpenseData,
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const targetRemainingChart = {
+    labels: Array.from({ length: targetRemainingData.length }, (_, i) => i + 1),
+    datasets: [
+      {
+        label: 'Target Remaining',
+        data: targetRemainingData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(255, 205, 86, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <div>
